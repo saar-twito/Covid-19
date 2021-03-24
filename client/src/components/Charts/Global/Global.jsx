2529049639717509340bf450f04abd26bdaa07a6
Saar Twito <saartwito49@gmail.com>
2021-03-24 21:04:20 +0200

    Remove unused code

--- client/src/components/Charts/Global/Global.jsx
+++ client/src/components/Charts/Global/Global.jsx
@@ -1,30 +1,28 @@
 import React from "react";
 import GlobeDoughnut from "./GlobeDoughnut";
 
-const Global = ({ info }) => {
-  return (
-    <div className="row mt-5">
-      <div
-        className="col-sm-12 col-md-6 col-lg-6 mt-5"
-        style={{ textAlign: "justify" }}
-      >
-        <h1 style={{ color: "grey" }}>Covid-19 around the world</h1>
-        <p>
-          The Covid-19 pandemic has reached almost every country in the world.
-          Its spread has left national economies and businesses counting the
-          costs, as governments struggle with new lockdown measures to tackle
-          the spread of the virus. Despite the development of new vaccines, many
-          are still wondering what recovery could look like.
-          <br />
-          <strong>In the diagram on the right</strong>, you can see the data on
-          the state of the corona in the world.
-        </p>
-      </div>
-      <div className="col-sm-12 col-md-6 col-lg-6 mt-5">
-        <GlobeDoughnut info={info} />
-      </div>
+const Global = ({ info }) => (
+  <div className="row mt-5">
+    <div
+      className="col-sm-12 col-md-6 col-lg-6 mt-5"
+      style={{ textAlign: "justify" }}
+    >
+      <h1 style={{ color: "grey" }}>Covid-19 around the world</h1>
+      <p>
+                The Covid-19 pandemic has reached almost every country in the world.
+          spread has left national economies and businesses counting the
+             as governments struggle with new lockdown measures to tackle
+              ad of the virus.Despite the development of new vaccines, many
+        are still wondering what recovery could look like.
+        <br />
+        <strong>In the diagram on the right</strong>, you can see the data on
+        the state of the corona in the world.
+      </p>
     </div>
-  );
-};
+    <div className="col-sm-12 col-md-6 col-lg-6 mt-5">
+      <GlobeDoughnut info={info} />
+    </div>
+  </div>
+);
 
 export default Global;
--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,18 +2,16 @@ import React from "react";
 
 // * Style
 import classes from "./style.module.css";
-const Footer = () => {
-  return (
-    <div className={classes.Container}>
-      <h1 style={{ color: "white" }}>Want to learn more?</h1>
-      <a
-        target="_blank"
-        href="https://www.who.int/health-topics/coronavirus#tab=tab_1"
-      >
-        Coronavirus - WHO | World Health Organization
-      </a>
-    </div>
-  );
-};
+const Footer = () => (
+  <div className={classes.Container}>
+    <h1 style={{ color: "white" }}>Want to learn more?</h1>
+    <a
+      target="_blank"
+      href="https://www.who.int/health-topics/coronavirus#tab=tab_1"
+    >
+      Coronavirus - WHO | World Health Organization
+    </a>
+  </div>
+);
 
 export default Footer;
